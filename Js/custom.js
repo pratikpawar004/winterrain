42be5606eec003d5802f8d941907115bfb60d6cc
pratikpawar004 <115889762+pratikpawar004@users.noreply.github.com>
2022-10-22 09:27:39 +0530

    Add files via upload

---
+++ Js/custom.js
@@ -0,0 +1,13 @@
+/*
+	Rocket Internet by TEMPLATE STOCK
+    templatestock.co @templatestock
+*/
+
+/* Header Full Screen */
+$(document).ready(function() {
+	$('#homeFullScreen').css({height:$(window).height(),width:$(window).width()});
+	$(window).resize(function(){
+        $('#homeFullScreen').css({height:$(window).height(),width:$(window).width()});
+    });
+
+});
