a37a89f24929aa46d5e3b9d1e7e8241a238e0d87
Pratik Pawar <115889762+pratikpawar004@users.noreply.github.com>
2023-05-27 20:11:10 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+
+
+/* Header Full Screen */
+$(document).ready(function() {
+	$('#homeFullScreen').css({height:$(window).height(),width:$(window).width()});
+	$(window).resize(function(){
+        $('#homeFullScreen').css({height:$(window).height(),width:$(window).width()});
+    });
+
+});
